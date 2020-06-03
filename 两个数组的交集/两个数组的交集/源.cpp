2815d15e18b7ae1d214344c6adb5a118ac74b5f1
yaogit666 <1397847010@qq.com>
2020-06-03 13:40:06 +0800

    6.3

---
+++ 两个数组的交集/两个数组的交集/源.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+	vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+		sort(nums1.begin(), nums1.end());
+		sort(nums2.begin(), nums2.end());
+		int i = 0;
+		int j = 0;
+		set<int> s;
+		while (i < nums1.size() && j < nums2.size())
+		{
+			if (nums1[i] == nums2[j])
+			{
+				s.insert(nums1[i]);
+				++i;
+				++j;
+			}
+			else if (nums1[i] > nums2[j])
+				++j;
+			else
+				++i;
+		}
+		vector<int>v(s.begin(), s.end());
+		return v;
+	}
+};
