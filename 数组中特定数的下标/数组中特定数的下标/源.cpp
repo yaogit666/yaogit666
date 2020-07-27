d978014073e20540d759ece953aa0d75c78211bc
yaogit666 <1397847010@qq.com>
2020-07-27 23:46:03 +0800

    7.27

---
+++ 数组中特定数的下标/数组中特定数的下标/源.cpp
@@ -0,0 +1,30 @@
+int search(vector<int>& nums, int target) {
+	if (nums.empty())
+		return -1;
+	int left = 0;
+	int right = nums.size() - 1;
+	while (left <= right)
+	{
+		int mid = left + ((right - left) >> 1);
+		if (nums[mid] == target)
+			return mid;
+
+		if (nums[mid] < nums[right]) {
+			if (nums[mid] < target && target <= nums[right]) {
+				left = mid + 1;
+			}
+			else {
+				right = mid - 1;
+			}
+		}
+		else if (nums[mid] >= nums[right]) {
+			if (nums[left] <= target && target < nums[mid]) {
+				right = mid - 1;
+			}
+			else {
+				left = mid + 1;
+			}
+		}
+	}
+	return -1;
+}
